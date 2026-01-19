71764618fc2e08b94d1219c0225dbce01e1b4269
SrinivasAccenture <sai.s.vavilapalli@accenture.com>
2026-01-19 10:35:40 +0000

    Update settings and urls for Codespace and localhost API support using CODESPACE_NAME env variable

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-!pu5n7d2rg4a#9)2lt!@=na+0x-+-31fkke-*+!(qt6zab-t_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
+ALLOWED_HOSTS.append('*')
 
 
 # Application definition
